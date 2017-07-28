4256983268481e4f4129418cc85abe02710033aa
sunnydgoswami <sunnydgoswami@gmail.com>
2017-07-28 13:48:25 -0400

    angularjs updates working

---
+++ js/services/Men.js
@@ -0,0 +1,53 @@
+var app = angular.module('product', []);
+
+app.controller('ProductCtrl', function($scope) {
+
+    $scope.details = [
+      { "name": '1 Million Intense Type: Men' },
+      { "name": 'Angel Type (Thierry Mugler): Men' },
+      { "name": 'Black Man Type: Men' },
+      { "name": 'Bleu De Chanel (Chanel) Type: Men' },
+      { "name": 'Black Ice Type: Men' },
+      { "name": 'Blue Jeans (Versace) Type: Men' },
+      { "name": 'Blue Nile Type for Unisex' },
+      { "name": 'Bora Bora (Liz Claiborne) Type: Men' },
+      { "name": 'Cool Water Deep (Davidoff) Type: Men' },
+      { "name": 'Creed Type: Men' },
+      { "name": 'Dolce Gabbana Type: Men' },
+      { "name": 'Drakkar  Type: Men' },
+      { "name": 'Fahrenheit Type: Men' },
+      { "name": 'Fierce (Abercrombie&Fitch) Type: Men' },
+      { "name": 'Giorgio (Armani) Type: Men' },
+      { "name": 'Guilty (Gucci) Type: Men' },
+      { "name": 'Issay Miyake Type: Men' },
+      { "name": 'J.P.Gaultier Le Male Type: Men' },
+      { "name": 'Opium (YSL) Type: Men' },
+      { "name": 'Oud Wood (Tom Ford) Type: Men' }
+    ];
+
+    $scope.names = [
+      { "name": 'Amber Romance Type' },
+      { "name": 'Addict Type (Dior)' },
+      { "name": 'Ambar White Type' },
+      { "name": 'Baby Power Type ' },
+      { "name": 'Black Orchid Type' },
+      { "name": 'Blue Nile Type for Unisex' },
+      { "name": 'Booty Call Type' },
+      { "name": 'Bora Bora (Liz Claiborne)' },
+      { "name": 'Burberry Type' },
+      { "name": 'Coco Chanel Type' },
+      { "name": 'Desire (Dunhill) Type' },
+      { "name": 'Flower Bomb (Viktor & Rolf) Type' },
+      { "name": '1 Million Type' },
+      { "name": 'Issey Miyake Type' },
+      { "name": 'Jamaican Fruit Type' },
+      { "name": 'Knowing Type' },
+      { "name": 'Opium (YSL) Type' },
+      { "name": 'Patchouli Type' },
+      { "name": 'Pink Sugar Sensual (Aquolina) Type' }
+    ];
+
+
+
+
+});
